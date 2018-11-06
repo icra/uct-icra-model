6dfda9185b0383dcebead606cff9fdd364ee238b
Lluís Bosch <holalluis@gmail.com>
2018-11-06 16:49:09 +0100

    added kinetic constants

---
+++ constants.js
@@ -0,0 +1,12 @@
+/*Constants*/
+
+//Activated sludge
+  const bH     = 0.24; //1/d | growth rate at 20ºC (standard)
+  const YH     = 0.45; //gVSS/gCOD
+  const fH     = 0.20; //         | tabled value
+  const f_iOHO = 0.15; //g_iSS/gX | fraction of inert solids in biomass
+
+//Nitrification
+  const µAm = 0.45; //1/d | growth rate at 20ºC
+  const Kn  = 1.0;  //mg/L as N at 20ºC
+  const bA  = 0.04; //1/d at 20ºC
