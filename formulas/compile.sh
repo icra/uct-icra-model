c4b7e62ff9ac99f0a31c28be7f8ca4b92ca0d12b
holalluis <holalluis@gmail.com>
2018-12-06 00:52:04 +0100

    pdf formulas update

--- formulas/compile.sh
+++ formulas/compile.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-#compile formulas.ms to formulas.pdf
-groff -t -e -ms formulas.ms -T pdf -K utf-8 > formulas.pdf
+#compile file to pdf
+
+file="formulas.ms"
+
+# groff options
+# -t  is for tbl (tables)
+# -e  is for eqn (equations)
+# -ms is file format
+# -T pdf is for pdf
+# -K utf-8 is for character encoding
+groff -t -e -ms "$file" -T pdf -K utf-8 > "$file.pdf"
