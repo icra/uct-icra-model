895ca8edb4efb92cf167e37893643ae29db91dc7
Lluís Bosch <holalluis@gmail.com>
2019-04-08 15:54:45 +0200

    update

--- src/run-all-tests.sh
+++ src/run-all-tests.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
 
+#execute all js files
 for file in $(ls *.js);do
-  echo $file
-  echo "--------";
+  echo -n "$file: | "
   node $file
-done
+  s=$? #exit status of last command
+  if [[ $s == 0 ]];then
+    echo "ok"
+  else
+    echo "there is a problem in $file"
+  fi
+done | column -t
